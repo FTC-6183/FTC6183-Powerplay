6d33df4b341c8bc8cee5deeed6165d585ab9b36f
sgu-101 <112212519+sgu-101@users.noreply.github.com>
2022-10-02 17:50:26 -0400

    new drive train
    slow mode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
@@ -13,18 +13,21 @@ public class DriveTrain extends LinearOpMode {
         DcMotorEx backRight = hardwareMap.get(DcMotorEx.class, "BR");
         DcMotorEx frontLeft = hardwareMap.get(DcMotorEx.class, "FL");
         DcMotorEx frontRight = hardwareMap.get(DcMotorEx.class, "FR");
-
+        double speedDivide = 1;
         waitForStart(); //waits for start
 
         while (opModeIsActive()){
             double y = (Math.pow(-gamepad1.left_stick_y,2))*Math.signum(-gamepad1.left_stick_y); //y value is inverted
             double x = (Math.pow(gamepad1.left_stick_x,2))*Math.signum(gamepad1.left_stick_x);
             double rx = gamepad1.right_stick_x;
-
-            double frontLeftSpd=y+x+rx; //y+x+rx
-            double frontRightSpd=y-x-rx;
-            double backLeftSpd=y-x+rx; //y-x+rx
-            double backRightSpd=y+x-rx;
+            
+            if (gamepad1.left_bumper){
+                speedDivide = 4;
+            }
+            double frontLeftSpd=(y+x+rx)/speedDivide; //y+x+rx
+            double frontRightSpd=(y-x-rx)/speedDivide;
+            double backLeftSpd=(y-x+rx)/speedDivide; //y-x+rx
+            double backRightSpd=(y+x-rx)/speedDivide;
 
             double denominator = Math.max(Math.abs(y)+Math.abs(x)+Math.abs(rx),1);
             frontRightSpd /= denominator;
