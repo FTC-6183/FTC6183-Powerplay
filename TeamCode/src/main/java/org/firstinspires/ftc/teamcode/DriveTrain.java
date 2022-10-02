7d78a78c01c566116cedb74baf444a58e77fc95c
sgu-101 <112212519+sgu-101@users.noreply.github.com>
2022-10-02 17:18:11 -0400

    drive train
    drive train

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@TeleOp
+public class DriveTrain extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException{
+        DcMotorEx backLeft = hardwareMap.get(DcMotorEx.class, "BL");
+        DcMotorEx backRight = hardwareMap.get(DcMotorEx.class, "BR");
+        DcMotorEx frontLeft = hardwareMap.get(DcMotorEx.class, "FL");
+        DcMotorEx frontRight = hardwareMap.get(DcMotorEx.class, "FR");
+
+        waitForStart(); //waits for start
+
+        while (opModeIsActive()){
+            double y = (Math.pow(-gamepad1.left_stick_y,2))*Math.signum(-gamepad1.left_stick_y); //y value is inverted
+            double x = (Math.pow(gamepad1.left_stick_x,2))*Math.signum(gamepad1.left_stick_x);
+            double rx = gamepad1.right_stick_x;
+
+            double frontLeftSpd=y+x+rx; //y+x+rx
+            double frontRightSpd=y-x-rx;
+            double backLeftSpd=y-x+rx; //y-x+rx
+            double backRightSpd=y+x-rx;
+
+            double denominator = Math.max(Math.abs(y)+Math.abs(x)+Math.abs(rx),1);
+            frontRightSpd /= denominator;
+            frontLeftSpd /= denominator;
+            backRightSpd /= denominator;
+            backLeftSpd /= denominator;
+
+            frontLeft.setPower(frontLeftSpd);
+            frontRight.setPower(frontRightSpd);
+            backLeft.setPower(backLeftSpd);
+            backRight.setPower(backRightSpd);
+            backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        }
+    }
+}
