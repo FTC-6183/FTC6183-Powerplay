37eaeeeff553f8d15217add0d0d18025b110a46a
sgu-101 <112212519+sgu-101@users.noreply.github.com>
2023-01-28 00:03:55 -0500

    bugless !! unless im stupid
    shoudl be final for now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmSlidesControlTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmSlidesControlTeleOp.java
@@ -126,52 +126,26 @@ public class ArmSlidesControlTeleOp {
                 }
                 else if (eTime.time()>1800&&((Math.abs(LSlides.getCurrentPosition()-LSlides.getTargetPosition()))<20)&&(Math.abs(VBMotor.getCurrentPosition()-VBMotor.getTargetPosition())<20)) {
                     if (back) { //whether the arm goes fully on the back
-                        armTarget = 700;
+                        armTarget = 800;
                     } else {
-                        armTarget = 686;
+                        armTarget = 786;
                     }
                      //finish arm movement
-                    VBMotor.setPower(0.04);
+                    VBMotor.setPower(0.12);
                     VBMotor.setTargetPosition(armTarget);
                     if (Math.abs(VBMotor.getCurrentPosition()-VBMotor.getTargetPosition())<20) {
-                        VBMotor.setPower(0);
-                        LSlides.setPower(0);
-                        RSlides.setPower(0);
-                            /*VBMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); //get ready for dpad finetuners
-                            LSlides.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-                            RSlides.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);*/
                         eTime.reset();
                         liftState = State.DEPOSIT;
                     }
                 }
                 break;
             case DEPOSIT:
-                    /*
-                    if (gamepad1.dpad_down){ //dpad finetuners
-                        LSlides.setPower(-0.07);
-                        RSlides.setPower(0.07);
-                        eTime.reset();
-                        adjusted = true;
-                    } else if (gamepad1.dpad_up){
-                        LSlides.setPower(0.07);
-                        RSlides.setPower(-0.07);
-                        eTime.reset();
-                        adjusted = true;
-                    } else {
-                        LSlides.setPower(0);
-                        RSlides.setPower(0);
-                    }
-                    if (gamepad1.dpad_left){
-                        VBMotor.setPower(-0.08);
-                        eTime.reset();
-                        adjusted = true;
-                    } else if (gamepad1.dpad_right) {
-                        VBMotor.setPower(0.08);
-                        eTime.reset();
-                        adjusted = true;
-                    } else {
-                        VBMotor.setPower(0);
-                    }*/
+                VBMotor.setPower(-0.2);
+                LSlides.setPower(0.2);
+                RSlides.setPower(-0.2);
+                VBMotor.setTargetPosition(armTarget);//back to zero position
+                LSlides.setTargetPosition(targetPos);
+                RSlides.setTargetPosition(-targetPos);
                 if ((lTrig>0.2)||(x>1)){ //deposit after some time, unless it is adjusted
                     Intake.setPower(-0.8); //deposit
                     if (x==0){
@@ -185,13 +159,13 @@ public class ArmSlidesControlTeleOp {
                             RSlides.setMode(DcMotor.RunMode.RUN_TO_POSITION);*/
                         Intake.setPower(0);
 
-                        LSlides.setPower(-0.2);
-                        RSlides.setPower(0.2);
+                        LSlides.setPower(-0.3);
+                        RSlides.setPower(0.3);
                         //VBMotor.setPower(((offset)*x)-0.2);
                         VBMotor.setPower(-0.1);
-                        VBMotor.setTargetPosition(525);//back to zero position
-                        LSlides.setTargetPosition(0);
-                        RSlides.setTargetPosition(0);
+                        armTarget = 525;
+                        targetPos = 0;
+
                         if ((lTrig>0.2)&&(Math.abs(VBMotor.getCurrentPosition()-VBMotor.getTargetPosition())<30)&&(Math.abs(LSlides.getCurrentPosition()-LSlides.getTargetPosition())<30)){
                             VBMotor.setPower(0);
                             VBMotor.setTargetPosition(0);
